c538eed953d656b983ef8d7d02a3a1c5f8d97347
hamzaabduljalil <hamza.abduljalil100@gmail.com>
2024-04-09 02:44:15 +0300

    first cmt

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import Footer from "./components/Footer/Footer";
 import Team from "./components/Team/Team";
 import WeAre from "./components/WeAre/WeAre";
 import Aos from "aos";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
-import { BrowserRouter } from "react-router-dom";
+
 function App() {
   useEffect(() => {
     Aos.init({ duration: 1600 });
   }, []);
+
   return (
     <div>
-      <BrowserRouter basename="/test2">
-        <Route path="/" element={<Home></Home>} />
-      </BrowserRouter>
-      <BrowserRouter basename="/test2">
-        <Route path="/Team" element={<Team></Team>} />
-        <Route path="/Contact" element={<Contact></Contact>} />
-        <Route path="/projects" element={<Projects></Projects>} />
-      </BrowserRouter>
-      <BrowserRouter basename="/test2">
-        <Route path="/we-are" element={<WeAre></WeAre>} />
-      </BrowserRouter>
-      <Footer></Footer>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/we-are" element={<WeAre />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
